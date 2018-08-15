27c6e32e8e2837e14f70ef324d6f090606b4fb59
Tiago Spadetto dos Santos <tianospadetto@gmail.com>
2018-08-15 00:29:20 -0300

    Menu e Login

---
+++ SmartIrrigation/app/src/main/java/br/com/smartirrigation/smartirrigation/activity/HomeActivity.java
@@ -0,0 +1,84 @@
+package br.com.smartirrigation.smartirrigation.activity;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import br.com.smartirrigation.smartirrigation.R;
+import br.com.smartirrigation.smartirrigation.fragments.HomeFragment;
+
+import static br.com.smartirrigation.smartirrigation.R.layout.menu_header;
+
+public class HomeActivity extends AppCompatActivity implements  NavigationView.OnNavigationItemSelectedListener{
+
+    private ActionBar actionBar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private FrameLayout container;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        Toolbar toolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+
+        actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+
+        drawerLayout = findViewById(R.id.main_drawer_layout);
+        navigationView = findViewById(R.id.main_navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+
+        mudaFragmetContainer(new HomeFragment());
+
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+        switch (item.getItemId()){
+            case R.id.nav_home:
+                fragment = new HomeFragment();
+                break;
+        }
+        drawerLayout.closeDrawers();
+
+        if(fragment != null){
+            mudaFragmetContainer(fragment);
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId() == android.R.id.home){
+            drawerLayout.openDrawer(GravityCompat.START);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void mudaFragmetContainer(Fragment fragment){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.main_container, fragment);
+        transaction.commit();
+    }
+}
--- SmartIrrigation/app/src/main/java/br/com/smartirrigation/smartirrigation/activity/LoginActivity.java
+++ SmartIrrigation/app/src/main/java/br/com/smartirrigation/smartirrigation/activity/LoginActivity.java
@@ -1,15 +1,33 @@
 package br.com.smartirrigation.smartirrigation.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import br.com.smartirrigation.smartirrigation.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private Button login_Button;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login_Button = findViewById(R.id.login_button);
+
+        login_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,
+                        HomeActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
